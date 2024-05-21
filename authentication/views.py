7077813657cc4f642fb605246267830a8612df75
jeremy <jeremychoo85@gmail.com>
2024-05-21 17:43:01 +0800

    oauth returns json response

--- authentication/views.py
+++ authentication/views.py
@@ -13,6 +13,9 @@ import requests
 import os
 import environ
 import json
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import JsonResponse
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -50,21 +53,35 @@ def get_user_info(access_token):
     return response.json()
 
 def oauth_callback(request):
-    code = request.GET.get('code')  # Get the authorization code from query parameters
+    code = request.GET.get('code')
     if code:
         access_token_response = exchange_code_for_token(code)
         if 'access_token' in access_token_response:
             access_token = access_token_response['access_token']
             user_info = get_user_info(access_token)  # Get user information using the token
-            return HttpResponse(f"Authorization successful! User info: {json.dumps(user_info)}")
+
+            # Handle or create the user in your Django application
+            User = get_user_model()
+            user, created = User.objects.get_or_create(username=user_info['login'], defaults={'email': user_info.get('email', '')})
+            if created:
+                user.set_unusable_password()  # Users logging in via OAuth won't use Django's password management
+                user.save()
+
+            # Create JWT tokens for the user
+            refresh = RefreshToken.for_user(user)
+            jwt_tokens = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
+
+            return JsonResponse(jwt_tokens)
         else:
             # Handle the error case
             error_details = access_token_response.get('details', {})
-            return HttpResponse(f"Failed to retrieve access token. Details: {error_details}")
+            return HttpResponse(f"Failed to retrieve access token. Details: {json.dumps(error_details)}")
     else:
         error = request.GET.get('error', 'Unknown error')
         return HttpResponse(f"Failed to authorize: {error}")
-
 
 def exchange_code_for_token(code):
     token_url = 'https://api.intra.42.fr/oauth/token'
