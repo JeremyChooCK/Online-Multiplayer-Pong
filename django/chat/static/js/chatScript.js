28c4d85fe68187d181b9b1564311b70da305ac60
kali <kali@kali.(none)>
2024-08-23 00:53:18 +0800

    implement block feature

--- django/chat/static/js/chatScript.js
+++ django/chat/static/js/chatScript.js
@@ -12,6 +12,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const chatSections = {};  // Store chat sections for each user
     
     const token = localStorage.getItem("accessToken");
+    const usernamesDiv = document.getElementById('usernames');
+    const chatHeader = document.getElementById('chat-header');
+    const chatHeaderText = document.getElementById('chat-header-text');
+    const chatInput = document.getElementById('chat-input-container');
+    const blockButton = document.getElementById('block-button');
 
     fetch('/chat/usernames/', {            
       method: "GET",
@@ -22,10 +27,6 @@ document.addEventListener('DOMContentLoaded', function () {
     })
       .then(response => response.json())
       .then(usernames => {
-        const usernamesDiv = document.getElementById('usernames');
-        const chatHeader = document.getElementById('chat-header');
-        const chatHeaderText = document.getElementById('chat-header-text');
-        const chatInput = document.getElementById('chat-input-container');
 
         usernames.forEach(username => {
           if (username !== currentUser) {
@@ -33,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
             chatTarget.textContent = username;
   
             chatTarget.onclick = () => {
+              console.log('click');
               // Deselect previous selection
               const previouslySelected = document.querySelector('.user-list p.selected');
               if (previouslySelected) {
@@ -43,21 +45,32 @@ document.addEventListener('DOMContentLoaded', function () {
               recipientId = username;
               chatHeaderText.textContent = recipientId;
   
-              // Show or hide chat-header
+              // Show or hide chat-header (bar above chat messages)
               if (chatHeaderText.textContent.trim() === '') {
-                chatHeader.classList.remove('d-block');
+                chatHeader.classList.remove('d-flex');
                 chatHeader.classList.add('d-none');
               } else {
                 chatHeader.classList.remove('d-none');
-                chatHeader.classList.add('d-block');
+                chatHeader.classList.add('d-flex');
               }
+
+              // set block button text based on blockArray. if user is blocked, text = 'Unblock'
+              if (blockArray.includes(recipientId)) {
+                blockButton.textContent = 'Click to Unblock';
+              } else{
+                blockButton.textContent = 'Click to Block';
+              }
+
   
-              // Switch chat container
+              // Switch chat container. div id = chat-messages-recipientId
+              
+              // hide all chat sections
               Object.values(chatSections).forEach(section => {
                 section.classList.add('d-none');
                 section.classList.remove('d-flex');
               });
-                            
+              
+              // if chat section not loaded, create it
               if (!chatSections[recipientId]) {
                 const chatMessagesDiv = document.createElement('div');
                 chatMessagesDiv.className = 'chat-messages flex-grow-1 p-3 overflow-auto';
@@ -65,20 +78,60 @@ document.addEventListener('DOMContentLoaded', function () {
                 chatSections[recipientId] = chatMessagesDiv;
                 document.querySelector('.chat-section').insertBefore(chatMessagesDiv, document.querySelector('.chat-input-container'));
               }
-              chatSections[recipientId].classList.remove('d-none');
-              chatSections[recipientId].classList.add('d-flex');
 
-              if (chatInput.classList.contains('d-none')) {
+              // show the recipient Id chat section
+              if (!blockArray.includes(recipientId)) {
+                chatSections[recipientId].classList.remove('d-none');
+                chatSections[recipientId].classList.add('d-flex');
+              }
+
+              if (chatInput.classList.contains('d-none') && !blockArray.includes(recipientId)) {
                 chatInput.classList.remove('d-none');
                 chatInput.classList.add('d-flex');
               }
             };
-  
             usernamesDiv.appendChild(chatTarget);
           }
         });
       });
-  
+
+    let blockArray = [];
+
+    blockButton.addEventListener('click', function() {
+      const indexOfRecipient = blockArray.indexOf(recipientId);
+      
+      if (indexOfRecipient === -1) {
+        // User is not blocked, so block them
+        blockArray.push(recipientId);
+        // change button text
+        blockButton.textContent = 'Click to Unblock';
+        chatHeaderText.textContent = recipientId + ' (BLOCKED)';
+        // remove chat input
+        chatInput.classList.remove('d-flex');
+        chatInput.classList.add('d-none');
+        // remove chat section
+        chatSections[recipientId].classList.remove('d-flex');
+        chatSections[recipientId].classList.add('d-none');
+        console.log(blockArray)
+      } else {
+        // User is blocked, so unblock them
+        blockArray.splice(indexOfRecipient, 1);
+        // change button text
+        blockButton.textContent = 'Click to Block';
+        chatHeaderText.textContent = recipientId;
+        // remove chat input
+        chatInput.classList.remove('d-none');
+        chatInput.classList.add('d-flex');
+        // remove chat section
+        chatSections[recipientId].classList.remove('d-none');
+        chatSections[recipientId].classList.add('d-flex');
+        console.log(blockArray)
+      }
+    });
+
+
+    // CHAT SOCKET
+
     const chatSocket = new WebSocket("wss://" + window.location.host + "/ws/chat/");
   
     function getTimeStamp(date) {
@@ -150,7 +203,7 @@ document.addEventListener('DOMContentLoaded', function () {
     chatSocket.onmessage = function (e) {
       const data = JSON.parse(e.data);
       console.log(data.sender, data.message);
-      if (data.sender && data.message) {
+      if (data.sender && data.message && !blockArray.includes(data.sender)) {
         if (!chatSections[data.sender]) {
           const chatMessagesDiv = document.createElement('div');
           chatMessagesDiv.className = 'chat-messages flex-grow-1 p-3 overflow-auto d-none';
